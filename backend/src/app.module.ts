d99901e5da2e39e6a89d031560a797ea3d754802
Sushanto Vharati <sushantovharati.sv@gmail.com>
2025-11-17 20:38:43 +0600

    Add

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,8 @@
 import { Module } from '@nestjs/common';
-import { UsersModule } from './users/users.module';
 import { AdminModule } from './admin/admin.module';
-import { StudentModule } from './student/student.module';
 
 @Module({
-  imports: [UsersModule, AdminModule, StudentModule],
+  imports: [ AdminModule],
   controllers: [],
   providers: [],
 })
--- backend/src/users/dto/user.dto.ts
+++
@@ -1,5 +0,0 @@
-export class UserDto {
-  name: string;
-  email: string;
-  role: string;
-}
